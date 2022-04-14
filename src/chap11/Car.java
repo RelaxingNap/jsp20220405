d853d7f4f3d30e8979f2ed5e64c717865f19c7bc
RelaxingNap <99159723+RelaxingNap@users.noreply.github.com>
2022-04-14 23:06:00 +0900

    update src

--- src/chap11/Car.java
+++ src/chap11/Car.java
@@ -1,34 +1,12 @@
 package chap11;
 
-<<<<<<< Updated upstream
-import java.util.List;
-=======
 import java.util.*;
->>>>>>> Stashed changes
 
 public class Car {
 	private String model;
 	private int price;
 	private boolean available;
 	private List<String> owners;
-<<<<<<< Updated upstream
-	
-	public Car() {
-		
-	}
-	
-	public Car(String model, int price) {
-		this.model = model;
-		this.price = price;
-	}
-	
-	public List<String> getOwners() {
-		return owners;
-	}
-	
-	public void setOwners(List<String> owners) {
-		this.owners = owners;
-=======
 	
 	public Car() {}
 	
@@ -43,11 +21,6 @@ public class Car {
 
 	public void setOwners(List<String> owners) {
 		this.owners = owners;
-	}
-
-	public boolean isAvailable() {
-		return available;
->>>>>>> Stashed changes
 	}
 	
 	public void setAvailable(boolean available) {
